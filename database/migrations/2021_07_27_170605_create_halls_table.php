6b4c4f0231a395056cb1ea2c8b18e78772954860
shishir75 <shishir.srdr16@gmail.com>
2021-07-27 23:15:39 +0600

    User, Role, Student, Hall, Session, Dept Model & Migration

--- database/migrations/2021_07_27_170605_create_halls_table.php
+++ database/migrations/2021_07_27_170605_create_halls_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateHallsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create( 'users', function ( Blueprint $table ) {
+        Schema::create( 'halls', function ( Blueprint $table ) {
             $table->bigIncrements( 'id' );
-            $table->string( 'name' );
-            $table->string( 'email' )->unique();
-            $table->string( 'password' );
-            $table->integer( "role_id" )->default( "4" );
-            $table->rememberToken();
+            $table->string( "name" );
+            $table->integer( "vacancy" )->default( 0 );
             $table->timestamps();
         } );
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists( 'users' );
+        Schema::dropIfExists( 'halls' );
     }
 }
--- database/migrations/2021_07_27_170642_create_depts_table.php
+++ database/migrations/2021_07_27_170642_create_depts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDeptsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create( 'users', function ( Blueprint $table ) {
+        Schema::create( 'depts', function ( Blueprint $table ) {
             $table->bigIncrements( 'id' );
-            $table->string( 'name' );
-            $table->string( 'email' )->unique();
-            $table->string( 'password' );
-            $table->integer( "role_id" )->default( "4" );
-            $table->rememberToken();
+            $table->string( "name" );
             $table->timestamps();
         } );
     }
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists( 'users' );
+        Schema::dropIfExists( 'depts' );
     }
 }
